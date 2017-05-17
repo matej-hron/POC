6a6a618cc91a85d3be327c86a7e1ce74218b2c88
CIS\m.hron <matej.hron@gmail.com>
2017-05-17 10:49:56 +0200

    easynetq

--- RabbitMQ.Producer1/Program.cs
+++ RabbitMQ.Producer1/Program.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Akka.Actor;
+using EasyNetQ;
 using RabbitMQ.Producer1.Tests.Actors;
+using RabbitMQ.Producer1.Tests.Messages;
 
 namespace RabbitMQ.Producer1
 {
@@ -13,8 +15,41 @@ namespace RabbitMQ.Producer1
         public static void Main()
         {
             //BasicPublishTest();
-            Run();
+            Run2();
         }
+
+        public static void Run2()
+        {
+            var bus = RabbitHutch.CreateBus("host=localhost;username=guest;password=guest;persistentMessages=true"); 
+
+            var publisherTask = Task.Run(() =>
+            {
+                Enumerable.Range(1, 10000).ToList().ForEach(i =>
+                {
+                    if(i%1000==0)
+                        Console.WriteLine($"publishing {i}");
+
+                    var message = new WorkMessage(i);
+                    bus.Publish(message);
+                });
+            });
+            //publisherTask.Wait();
+            var tasks = Enumerable.Range(1, 10).Select(i => Task.Run(() =>
+            {
+
+                using (var subscriptionResult = bus.SubscribeAsync<WorkMessage>($"sub{i}", w => Task.Run(() =>
+                {
+                    if (w.Id % 1000 == 0)
+                        Console.WriteLine($"receiving: {DateTime.Now.ToShortTimeString()}-{w.Id}");
+                })))
+                {
+                    Task.Delay(TimeSpan.FromMinutes(1)).Wait();
+                }
+            }));
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
 
         private static void Run()
         {
@@ -25,7 +60,7 @@ namespace RabbitMQ.Producer1
 
             Task.Delay(100).Wait();
 
-            CreateSubscribers(system, 10);
+            CreateSubscribers(system, 1);
 
 
             Console.WriteLine("Publishing");
